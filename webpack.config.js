fc70ff602f3513e8cf8d7598eb92fac01062756c
Dima-f <dafw@live.ru>
2018-02-28 23:39:00 +0200

    webpack with min & babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,16 @@ module.exports = {
     module:{
         rules: [
                 {
+                  test: /\.js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                  }
+                },
+                {
                     test: /\.(html)$/,
                     use: {
                         loader: 'html-loader',
@@ -31,7 +41,7 @@ module.exports = {
         ]
     },
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
         new WriteFilePlugin(),
         new HtmlWebpackPlugin({
             template: './index.html'
