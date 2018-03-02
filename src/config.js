f67168a45ee9b0dcbd5b9549cdcd03d1b4707ea5
Dima <dafw@live.ru>
2018-03-02 18:05:06 +0200

    before weekend

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ module.exports = {
     //url:'https://localhost:3000',
     
     //volya express server
-    url:'http://93.72.190.134:3000',
+    //url:'http://93.72.190.134:3000',
     
     //remote s.bpi.in.ua erpjs server
-    //url:'http://s.bpi.in.ua:3001/api/form-registration'
+    url:'http://s.bpi.in.ua:3001/api/form-registrations'
 };
