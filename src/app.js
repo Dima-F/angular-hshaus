c7c4c39748b99329ece8132ecf68452dc950057e
Dima-f <dafw@live.ru>
2018-03-06 00:31:55 +0200

    simple routing start

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 const angular = require('angular');
 require('angular-countries');
-const hshaus = angular.module('hshaus',['ngCountries']);
+require('angular-route');
+const hshaus = angular.module('hshaus',['ngCountries','ngRoute']);
+hshaus.config(['$routeProvider','$locationProvider',
+    function config($routeProvider,$locationProvider) {
+      $locationProvider.hashPrefix('!');
+      $routeProvider.
+        when('/', {
+          template: `<div class="row">
+                      <form class="needs-validation" name="mainForm" novalidate>
+                            <services services="main.services"></services>
+                            <person who="main.applicant"></person>
+                            <company company="main.company"></company>
+                            <structure structure="main.structure"></structure>
+                            <question question="main.question"></question>
+                            <p class="text-danger" ng-show="mainForm.$invalid">To enable submit button, please enter all required fields (with *) and check email and date fields validation!</p>
+                            <button class="btn btn-secondary btn-lg" type="submit" data-toggle="modal" data-target="#myModal" ng-disabled="mainForm.$invalid">Submit</button>
+                      </form>
+                  </div>
+                  <modal services="main.services" applicant="main.applicant" company="main.company" structure="main.structure" question="main.question" sendData="main.sendData"></modal>`
+        }).
+        when('/error', {
+          template: '<h1>Error</h1>'
+        }).
+        otherwise('/');
+    }
+  ]);
+
+
+
 require('./css/my.css');
 require('./css/radio.css');
 require('./css/checkbox.css');
