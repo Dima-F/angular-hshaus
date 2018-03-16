a96ebb01c37b3c0c26e28d776c9074fe422bbf79
Dima <dafw@live.ru>
2018-03-16 11:53:05 +0200

    test config fixing

--- src/mainController.js
+++ src/mainController.js
@@ -24,7 +24,7 @@ module.exports = ['$location','$scope', function($location, $scope){
         turnover:""
     };
     this.structure = new Structure();
-    /*
+    
     //now lets add one director, beneficial and shareholder to structure
     let firstDirector = new Person();
     firstDirector.isDirector=true;
@@ -35,7 +35,7 @@ module.exports = ['$location','$scope', function($location, $scope){
     let firstShareholder = new Person();
     firstShareholder.isShareholder=true;
     this.structure.people.push(firstShareholder);
-    */
+    
     this.question = {
         politicallyExposed:"No"
     };
